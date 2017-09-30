e6f704ab8fad32891a08af0c1974b35f50a06f28
dingtingting <dingtingting@gomeholdings.com>
2017-09-30 10:15:50 +0800

    xss

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.set('X-XSS-Protection', 'default-src')
+  res.set('Content-Security-Policy', 0)
+  res.render('index', { title: 'Express' , xss: req.query.xss});
+});
+
+module.exports = router;
